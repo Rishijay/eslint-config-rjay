d9090565153505549fc43e77f123d4e6d4ca988b
Rishijay Shrivastava <rishijay.shrivastava@gmail.com>
2023-01-19 01:15:03 +0530

    Initial commit

---
+++ .eslintrc.js
@@ -0,0 +1,31 @@
+module.exports = {
+  extends: [
+    "airbnb",
+    "eslint:recommended",
+    "plugin:import/errors",
+    "plugin:react/recommended",
+    "plugin:jsx-a11y/recommended",
+    "plugin:prettier/recommended",
+    "prettier/react",
+  ],
+  plugins: ["react-hooks", "check-file"],
+  rules: {
+    quotes: ["error", "double"],
+    "linebreak-style": ["error", "windows"],
+    camelcase: ["error", { properties: "always" }],
+    "check-file/filename-naming-convention": [
+      "error",
+      {
+        "*/.{js,ts}": "CAMEL_CASE",
+        "*.{jsx,tsx}": "PASCAL_CASE",
+      },
+    ],
+    "check-file/folder-naming-convention": [
+      "error",
+      {
+        "src/**/": "CAMEL_CASE",
+        "mocks/*/": "KEBAB_CASE",
+      },
+    ],
+  },
+};
