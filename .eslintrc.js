b22e0af5f3575ac79dc4e5a327f2c3bae5789dba
Rishijay Shrivastava <rishijay.shrivastava@gmail.com>
2023-01-20 18:23:45 +0530

    removed check file - v0.1.1

--- .eslintrc.js
+++ .eslintrc.js
@@ -17,24 +17,10 @@ module.exports = {
     sourceType: 'module',
   },
   plugins: [
-    "check-file",
     "react",
   ],
   rules: {
     quotes: 0,
     'react/react-in-jsx-scope': 0,
-    "check-file/filename-naming-convention": [
-      "error",
-      {
-        "**/*.{jsx,tsx}": "PASCAL_CASE",
-        "**/*.{js,ts}": "CAMEL_CASE",
-      },
-    ],
-    "check-file/folder-naming-convention": [
-      "error",
-      {
-        "src/**/": "CAMEL_CASE",
-      },
-    ],
   },
 };
