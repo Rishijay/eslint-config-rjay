09b6dc825477bbb811fe582651e8d914960100ee
Rishijay Shrivastava <rishijay.shrivastava@gmail.com>
2023-05-07 14:09:59 +0530

    v0.0.2 - Custom rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,24 +3,38 @@ module.exports = {
     browser: true,
     es2021: true,
   },
-  extends: [
-    'plugin:react/jsx-runtime',
-    'airbnb',
-  ],
+  extends: ["plugin:react/jsx-runtime", "airbnb"],
   overrides: [
     {
       files: ["*.jsx", "*.js", "*.tsx", "*.ts"],
     },
   ],
   parserOptions: {
-    ecmaVersion: 'latest',
-    sourceType: 'module',
+    ecmaVersion: "latest",
+    sourceType: "module",
   },
-  plugins: [
-    "react",
-  ],
+  plugins: ["react"],
+  ignorePatterns: ["index.js", "*.test.js", "index.*.js", "dist/*", "public/*"],
   rules: {
+    "file-naming": "error",
+    "comments-above": "error",
     quotes: 0,
-    'react/react-in-jsx-scope': 0,
+    "linebreak-style": 0,
+    "react/react-in-jsx-scope": 0,
+    "object-curly-newline": 0,
+    "comma-dangle": 0,
+    "arrow-parens": 0,
+    "no-param-reassign": 0,
+    "react/jsx-props-no-spreading": 0,
+    "jsx-a11y/no-static-element-interactions": 0,
+    "jsx-a11y/click-events-have-key-events": 0,
+    "react/function-component-definition": [
+      2,
+      {
+        namedComponents: ["arrow-function", "function-declaration"],
+        unnamedComponents: "arrow-function",
+      },
+    ],
+    "variable-naming": ["error", { exceptionFile: ".variableignore" }],
   },
 };
